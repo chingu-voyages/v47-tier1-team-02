98f820b4e6d261fdda58f04434a4726c7c047a4a
TerminalC3 <keerthikumar.m10@gmail.com>
2024-01-23 21:03:32 +0530

    Function for fetching task form data and converting it to json object

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 // Ensure use of Airbnb Javascript style guide.
+
+function openTaskForm() {
+  document.getElementById('task-window').style.display = 'flex';
+}
+
+function closeTaskForm() {
+  document.getElementById('task-window').style.display = 'none';
+}
+
+const taskForm = document.getElementById('task-form');
+
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const category = document.getElementById('category').value;
+  const activity = document.getElementById('activity').value;
+  const taskName = document.getElementById('task-name').value;
+  const taskDesc = document.getElementById('task-desc').value;
+  const days = document.getElementById('days').value;
+
+  const newTask = {
+    categoryName: category,
+    activityTypes: [
+      {
+        activityName: activity,
+        Tasks: [
+          {
+            taskName: taskName,
+            taskDescription: taskDesc,
+            days: [days],
+          },
+        ],
+      },
+    ],
+  };
+  console.log(newTask);
+});
