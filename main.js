df69ecfc6262e1ea558520ebbac20fe077d94b26
TerminalC3 <keerthikumar.m10@gmail.com>
2024-01-26 00:33:53 +0530

    Storing checkbox status in json file

--- main.js
+++ main.js
@@ -1,4 +1,43 @@
-// Ensure use of Airbnb Javascript style guide.
+// Follows Airbnb Javascript style guide.
+
+const jsonString = [
+  {
+    categoryName: 'Routine activities',
+    activityTypes: [
+      {
+        activityName: 'Projects',
+        Tasks: [
+          {
+            taskName: 'Ontrack',
+            taskDescription: 'Create json data handling',
+            days: [
+              'Tuesday',
+            ],
+            completion: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    categoryName: 'Studying',
+    activityTypes: [
+      {
+        activityName: 'Node js course',
+        Tasks: [
+          {
+            taskName: 'Read bookmarked article',
+            taskDescription: 'Go through all articles in bookmark',
+            days: [
+              'Monday',
+            ],
+            completion: [],
+          },
+        ],
+      },
+    ],
+  },
+];
 
 function dateFormat(date) {
   const dd = String(date.getDate()).padStart(2, '0');
@@ -19,27 +58,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
   daysOfWeek.forEach((day) => {
     const dayDiv = document.createElement('div');
+
+    // Division for all components of the day
     dayDiv.classList.add('days');
 
     const dayHeader = document.createElement('div');
+
+    // Division for day name, date and add task button
     dayHeader.classList.add('day-header');
+
+    const fDate = dateFormat(date);
+
     dayHeader.innerHTML = `
             <button type="button">+</button>
-            <span class="day-name">${day} (${dateFormat(date)})</span>
+            <span class="day-name">${day} (${fDate})</span>
         `;
+
     date.setDate(date.getDate() + 1);
+
     const taskList = document.createElement('div');
+
+    // Division for task name and checkbox
     taskList.classList.add('task-list');
 
-    for (let i = 1; i <= 2; i += 1) {
-      const taskElement = document.createElement('div');
-      taskElement.classList.add('task-element');
-      taskElement.innerHTML = `
-                <label class="checkbox-label" for="${day}${i}-checkbox"> Task ${i}</label>
-                <input type="checkbox" id="${day}${i}-checkbox" name="task-checkbox" value="checked">
-            `;
-      taskList.appendChild(taskElement);
-    }
+    /* Find each task's day in json and if it is same as the day of element under construction.
+    If it is, add the task name under the constructed header. */
+    jsonString.forEach((category) => {
+      category.activityTypes.forEach((activityType) => {
+        activityType.Tasks.forEach((task) => {
+          task.days.forEach((dayN) => {
+            if (dayN === day) {
+              const taskElement = document.createElement('div');
+
+              // Division for individual task name and checkbox
+              taskElement.classList.add('task-element');
+
+              taskElement.innerHTML = `
+                  <label class="checkbox-label" for="${date}-${task.taskName}-checkbox"> ${task.taskName} </label>
+                  <input type="checkbox" id="${date}-${task.taskName}-checkbox" name="task-checkbox" value="checked" onchange="checkboxStore(${date}-${task.taskName}-checkbox)">
+              `;
+              taskList.appendChild(taskElement);
+            }
+          });
+        });
+      });
+    });
 
     dayDiv.appendChild(dayHeader);
     dayDiv.appendChild(taskList);
@@ -47,3 +110,27 @@ document.addEventListener('DOMContentLoaded', () => {
     daysContainer.appendChild(dayDiv);
   });
 });
+
+function checkboxStore(id) {
+  const date = id.slice(0,10);
+  const name = id.slice(11, -9);
+
+  jsonString.forEach((category) => {
+    category.activityTypes.forEach((activityType) => {
+      activityType.Tasks.forEach((task) => {
+        if (task.taskName === name) {
+          const index = task.completion.indexOf(date);
+          if (index !== -1) {
+            task.completion.splice(index, 1);
+          }
+          else {
+            task.completion.push(date);
+          }
+        }
+        // const newTask = { ...task };
+        // newTask.completion = !(newTask.completion);
+      });
+    });
+  });
+  console.log(jsonString);
+}
