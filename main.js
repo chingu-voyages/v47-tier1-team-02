58a3f410ece30f6babc5aac2d2999163ebccc4eb
TerminalC3 <keerthikumar.m10@gmail.com>
2024-01-26 21:57:49 +0530

    Formatting and comments

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 // Ensure use of Airbnb Javascript style guide.
 const tasksJson = [];
+
+// eslint-disable-next-line no-unused-vars
 function openTaskForm() {
   document.getElementById('task-window').style.display = 'flex';
 }
 
+// eslint-disable-next-line no-unused-vars
 function closeTaskForm() {
   document.getElementById('task-window').style.display = 'none';
 }
 
 const taskForm = document.getElementById('task-form');
 
+// eslint-disable-next-line no-unused-vars
 function createJson() {
   const category = document.getElementById('category').value;
   const activity = document.getElementById('activity').value;
@@ -33,6 +37,6 @@ function createJson() {
     ],
   };
   tasksJson.push(newTask);
-  console.log(JSON.stringify(tasksJson));
+  // console.log(JSON.stringify(tasksJson));
   taskForm.reset();
 }
