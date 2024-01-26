807ec2809eece7a68cf3deb53fdb35ad0f0a742e
andreiolteanu555 <andreiolteanu2014@gmail.com>
2024-01-26 18:50:16 +0200

    Mobile header
    I created the basic elements of the header for mobile.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // Ensure use of Airbnb Javascript style guide.
+
+/* activate toggle menu */
+document.addEventListener("DOMContentLoaded", function () {
+    let navToggle = document.querySelector(".nav-toggle");
+    let navMenu = document.getElementById("nav-menu");
+
+    navToggle.addEventListener("click", function () {
+       navMenu.classList.toggle("active");
+    });
+ });
