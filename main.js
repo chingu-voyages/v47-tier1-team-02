b2ec11f3ef6945cf7d3565007d6af11e7b877749
andreiolteanu555 <andreiolteanu2014@gmail.com>
2024-02-02 22:57:20 +0200

    Added the search container

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
        navMenu.classList.toggle("active");
     });
  });
+
+ /* search functionality */
+ document.getElementById('searchInput').addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+      event.preventDefault(); 
+      performSearch();
+  }
+});
+
+function performSearch() {
+  let query = document.getElementById('searchInput').value;
+  console.log('Searching for:', query);
+}
